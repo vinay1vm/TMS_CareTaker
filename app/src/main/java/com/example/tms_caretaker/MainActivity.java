5e0d7674fa27b3a5da322699a68c49d086ccf17e
vinay <vinayvm67@gmail.com>
2023-01-02 14:58:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tms_caretaker/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.tms_caretaker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+
+}
